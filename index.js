db0544e2522de8e97132fe6c2abfad41fc842317
steadfast-method-9892 <dong.jason86@gmail.com>
2020-08-30 02:02:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function whisper(string) {
 return string.toLowerCase()
 }
 function logShout(string) {
-consol.log(string).toUpperCase()
+consol.log(string.toUpperCase())
 }
 function logWhisper(string) {
 return consol.log(string).toLowerCase()
